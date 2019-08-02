c6f082a4e9d5af23939395f353c02ef05b710f4e
sebmar19 <53358454+sebmar19@users.noreply.github.com>
2019-08-02 09:49:27 -0500

    Add files via upload
    Proyecto de Da Vinci
    En la hoja de estilos, en la línea 376 está la clase que tiene el mask-image que es la propiedad con la que hice la transparencia

---
+++ da vinci/js/main-davinci.js
@@ -0,0 +1,352 @@
+// JavaScript Document
+var obrasTimeline={
+	"1":{
+		"nombre":"Tobias y el Ángel",
+		"fecha":"1473",
+        "descripcion":"En la escena, el Arcángel Rafael le da a Tobías una ‘fórmula’ para que su padre recupere la visión. Fue pintada por Andrea del Verrocchio, pero se debate la posibilidad de que Leonardo Da Vinci (su alumno) haya intervenido en su producción."
+	},
+	"2":{
+		"nombre":"Anunciación",
+		"fecha":"1474",
+        "descripcion":"La pintura muestra al Arcángel Gabriel anunciando a la virgen María que concebirá de forma milagrosa a un niño, Jesús. Es considerada uno de los primeros trabajos atribuidos a Da Vinci, quien la realizó mientras estudiaba con Andrea del Verrocchio."
+	},
+	"3":{
+		"nombre":"La Virgen del clavel",
+		"fecha":"1475",
+        "descripcion":"En esta obra, la virgen María le entrega al niño Jesús un clavel, uno de los símbolos de la Pasión de Cristo. Es una de las primeras obras de Leonardo Da Vinci en la que es notable la inclinación del artista por el realismo y el detalle."
+	},
+	"4":{
+		"nombre":"Bautismo de Cristo",
+		"fecha":"1476",
+        "descripcion":"En su estudio, Andrea del Verrocchio pintó esta comisión de los monjes de la Iglesia San Salvi de Florencia. Representa a San Juan Bautista durante el bautismo de Jesucristo. Leonardo Da Vinci, aprendiz de Verrocchio, lo ayudó a terminar la obra."
+	},
+	"5":{
+		"nombre":"Ginevra d’Benci",
+		"fecha":"1478",
+        "descripcion":"Retrata a Ginevra d’Benci, una joven aristócrata del siglo XV en Florencia que, al posar para este retrato, probablemente tenía 16 años. La mujer tiene las cejas afeitadas, como solían tenerlas las mujeres florentinas de la época."
+	},
+	"6":{
+		"nombre":"La Virgen del clavel",
+		"fecha":"1479",
+        "descripcion":"En esta obra, la virgen María le entrega al niño Jesús un clavel, uno de los símbolos de la Pasión de Cristo. Es una de las primeras obras de Leonardo Da Vinci en la que es notable la inclinación del artista por el realismo y el detalle."
+	},
+	"7":{
+		"nombre":"Adoración de la María",
+		"fecha":"1481",
+        "descripcion":"Esta pintura, que Da Vinci no alcanzó a finalizar, fue una comisión de los monjes agustinos de San Donato de Florencia. Muestra a la virgen María con el niño Jesús en brazos. Se ha especulado sobre la presencia de Leonardo en la escena del cuadro."
+	},
+	"8":{
+		"nombre":"San Jerónimo",
+		"fecha":"1482",
+        "descripcion":"Esta obra muestra uno de los personajes más impactantes que pintó Da Vinci, especialmente por la contorsión del rostro de San Jerónimo y la fina capa de piel que cubre sus músculos tensos. Da Vinci no pudo finalizarla antes de mudarse a Milán. "
+	},
+	"9":{
+		"nombre":"Retrato de músico",
+		"fecha":"1485",
+        "descripcion":"Es uno de los pocos retratos de hombres que realizó Da Vinci. Aunque se creía que retrataba a Ludovico el Moro (duque de Milán), se identificó en el siglo XX que se trataba de un músico."
+	},
+	"10":{
+		"nombre":"La dama del armiño",
+		"fecha":"1490",
+        "descripcion":"Da Vinci pintó este retrato de Cecilia Gallerani, amante de Ludovico Sforza (duque de Milán), mientras trabajaba para él. Es una de las obras más importantes del arte occidental; resistió a muchos traslados y robos a lo largo de los siglos."
+	},
+	"11":{
+		"nombre":"Virgen de las rocas",
+		"fecha":"1490",
+        "descripcion":"Esta pintura, considerada una obra maestra, fue encargada para la iglesia de San Francesco Grande en Milán. Muestra la Encarnación con la virgen María, Jesucristo y San Juan Bautista. Existe otra versión de la tabla en la Galería Nacional de Londres."
+	},
+	"12":{
+		"nombre":"Dama de la corte de Milán",
+		"fecha":"1497",
+        "descripcion":"Se ha discutido sobre si se trataba del retrato de Lucrezia Crivelli, una amante de Ludovico Sforza (duque de Milán), o de Isabela de Aragón. Tiene un estilo mucho más propio de un retrato rígido de la Corte que de la técnica natural de Da Vinci."
+	},
+	"13":{
+		"nombre":"La última cena",
+		"fecha":"1498",
+        "descripcion":"Es una de las obras de arte más famosas del mundo. Con esta versión de la Última Cena, Da Vinci revolucionó la forma de concebir a los personajes religiosos, alejándolos de su lado más santificado para representarlos de una forma humana."
+	},
+	"14":{
+		"nombre":"Sala de las tablas de madera",
+		"fecha":"1499",
+        "descripcion":"Realizada en las paredes y techos del Castillo Sforza (Milán), exhibe una técnica de “engaño al ojo” para representar elementos de la naturaleza tangibles Ha sido restaurada en varias ocasiones desde 1902 hasta la actualidad."
+	},
+	"15":{
+		"nombre":"La batalla de Anghiari",
+		"fecha":"1506",
+        "descripcion":"La obra fue pintada originalmente para un muro del Salón de los Quinientos del Palazzo Vecchio de Florencia. La obra desapareció en 1563 tras una remodelación; Pedro Pablo Rubens creó una copia en 1603 con ayuda de bocetos viejos de Da Vinci."
+	},
+	"16":{
+		"nombre":"Virgen de la rueca",
+		"fecha":"1507",
+        "descripcion":"Hace parte de un grupo de tres pinturas similares atribuidas a Da Vinci. La original (hoy perdida) retrata a la Virgen María con el Niño Jesús en brazos, mirando con nostalgia una rueca que sostiene la Virgen. Esta obra haría parte de las copias."
+	},
+	"17":{
+		"nombre":"Salvador del mundo",
+		"fecha":"1507",
+        "descripcion":"La obra muestra a Cristo dando una bendición con la mano derecha levantada y dedos cruzados mientras sostiene una esfera cristalina, que representa la esfera celeste de los cielos. Es la pintura más costosa del mundo, vendida por 450 millones de dólares."
+	},
+	"18":{
+		"nombre":"La virgen de las rocas",
+		"fecha":"1508",
+        "descripcion":"Esta pintura, considerada una obra maestra, fue encargada para la iglesia de San Francesco Grande en Milán. Muestra la Encarnación con la virgen María, Jesucristo y San Juan Bautista. Existe otra versión de la tabla en la Galería Nacional de Londres."
+	},
+	"19":{
+		"nombre":"Leda y el cisne",
+		"fecha":"1508",
+        "descripcion":"El mito griego de Leda y el cisne fue estudiado por Da Vinci, quien llegó a pintar una obra en la que se mostraba a una Leda desnuda de pie, abrazando al cisne, junto a dos parejas de bebés gemelos acostados sobre cáscaras de huevo rotas. "
+	},
+	"20":{
+		"nombre":"San Juan Bautista",
+		"fecha":"1516",
+        "descripcion":"Es una de las últimas obras del maestro. En ella se ve al santo retratado a medio cuerpo, sosteniendo una cruz de junco con la mano izquierda, vestido de pieles y señalando al cielo con la mano derecha mientras sonríe de manera enigmática."
+	},
+	"21":{
+		"nombre":"Monalisa",
+		"fecha":"1516",
+        "descripcion":"También conocida como “La Gioconda”, es probablemente el cuadro más famoso del mundo. Pintado con “esfumado”, retrata a una mujer sentada con los brazos sobre su regazo. Fue robada en 1911 del Museo Louvre durante dos años. "
+	},
+	"22":{
+		"nombre":"San Juan Bautista",
+		"fecha":"1516",
+        "descripcion":"Se presume que esta obra fue realizada por un seguidor de Da Vinci con base en un dibujo hecho por el artista y que años después fue alterada para que sirviera como Baco, dios griego del vino."
+	},
+	"23":{
+		"nombre":"La Virgen y el niño",
+		"fecha":"1517",
+        "descripcion":"Se asume que fue un encargo del rey Luis XII de Francia para agradecer a santa Ana el nacimiento de su hija única, Claude. En ella se ve a Santa Ana, a su hija, la Virgen María, y al Niño Jesús que abraza un cordero -su Pasión-."
+	}
+	
+};
+
+
+var scrollPos=0;
+var scrollVel=179;
+
+var scrollNum=1;
+
+$(document).ready(function(){
+	pruebaObras();
+	
+	$(".btn-der").on("click",moverDer);
+	$(".btn-izq").on("click",moverIzq);
+	$(".btn-top").on("click",moverTop);
+	$(".btn-cerrar").on("click",moverCentro);
+	$(".btn-obra").on("click",mostrarLightbox);
+	$(".light-cerrar").on("click",ocultarLightbox);
+	$(".flecha-der").on("mousedown",scrollLineaIzq).bind("mouseup mouseleave",scrollStop);
+    $(".flecha-vitru-der").on("click",moverScrollInfluenciasDer);
+    $(".flecha-vitru-izq").on("click",moverScrollInfluenciasIzq);
+	$(".flecha-izq").on("mousedown",scrollLineaDer).bind("mouseup mouseleave",scrollStop);
+	$(".scroll-btn").on("click",scrollInfluencias);
+	$(".btn-influencia").on("click",influenciaSelect);
+    $(".da-vinci-pop").on("click",mostrarVitru);
+    $(".cerrar-pop").on("click",ocultarVitru);
+    $(".imagen-inedit").on("click",mostrarInedit);
+    $(".cerrar-inedit").on("click",ocultarInedit);
+	
+	
+});
+
+
+
+function pruebaObras(){
+	
+	
+	for(var i=1;i<=23;i++){
+		var copia='<div class="obra"><div class="obra-hover"><div class="filtro-hover"><p class="obra-fecha"></p><p class="obra-nombre"></p><div data-num="'+i+'" class="btn-obra">+</div></div></div></div>';
+		$(".linea-obras").append(copia);
+	}
+	
+	for(var i=1;i<=23;i++){
+		$(".obra:nth-child("+i+")").css("background","url(https://embed.eltiempo.digital/infografias/2019/04/davinci/desktop/img/fotos-timeline/fondo"+i+".jpg)");
+		
+		$(".obra:nth-child("+i+")").css("background-size","cover");
+		
+		var fecha= obrasTimeline[i]["fecha"];
+		
+		var nombre= obrasTimeline[i]["nombre"];
+		
+		$(".obra:nth-child("+i+") .obra-fecha ").html(fecha);
+		
+		$(".obra:nth-child("+i+") .obra-nombre ").html(nombre);
+	}
+	
+	
+}
+
+var timeOut;
+
+function scrollLineaDer(){
+	if(scrollPos<320){
+		scrollPos += scrollVel;
+	
+	$(".linea-obras").animate({"marginLeft":scrollPos+"px"},300);
+		$(".linea-fechas").animate({"marginLeft":scrollPos+"px"},300);
+	
+	
+	 timeOut = setInterval(function(){
+		 
+		 if(scrollPos<320){
+			 scrollPos += scrollVel;
+	
+	$(".linea-obras").animate({"marginLeft":scrollPos+"px"},300); 
+	$(".linea-fechas").animate({"marginLeft":scrollPos+"px"},300); 
+			 
+		 }
+		 
+		
+      console.log("i++");
+    }, 300);
+	}
+	
+}
+function scrollLineaIzq(){
+	
+	var obras= document.getElementsByClassName("obra");
+	var limitIzq = (obras.length-5)*(-179);
+	if(scrollPos>limitIzq){
+		
+	scrollPos -= scrollVel;
+		$(".linea-obras").animate({"marginLeft":scrollPos+"px"},300);
+		$(".linea-fechas").animate({"marginLeft":scrollPos+"px"},300);
+	
+	
+	
+	timeOut = setInterval(function(){
+      console.log("i++");
+		
+		if(scrollPos>limitIzq){
+			scrollPos -= scrollVel;
+		$(".linea-obras").animate({"marginLeft":scrollPos+"px"},300);
+			$(".linea-fechas").animate({"marginLeft":scrollPos+"px"},300);
+			
+		}
+		
+		
+    }, 300);
+	}
+}
+
+function scrollStop(){
+	scrollPos -= 0;
+	clearInterval(timeOut);
+}
+
+function moverDer(){
+	$(".vitru-centro").animate({"left":"-100%"},200);
+	$(".vitru-dato-inicial").fadeOut(200);
+	$(".vitru-dato").fadeOut(200,function(){
+		$(".vitru-dato img").attr("src","https://embed.eltiempo.digital/infografias/2019/04/davinci/desktop/img/influencias/DaVinci_Florencia.png")
+	$(".vitru-dato h6").html("Florencia");
+	$(".vitru-dato p").html("");
+	$(".vitru-dato").fadeIn(200);
+		
+	});
+}
+
+function moverIzq(){
+	$(".vitru-centro").animate({"left":"100%"},200);
+	$(".vitru-dato-inicial").fadeOut(200);
+	$(".vitru-dato").fadeOut(200,function(){
+		$(".vitru-dato img").attr("src","https://embed.eltiempo.digital/infografias/2019/04/davinci/desktop/img/influencias/DaVinci_milan.png")
+	$(".vitru-dato h6").html("Milán");
+	$(".vitru-dato p").html("");
+	$(".vitru-dato").fadeIn(200);
+		
+	});
+	
+}
+
+function moverTop(){
+	$(".vitru-centro").animate({"top":"100%"},200);
+	$(".vitru-dato-inicial").fadeOut(200);
+	$(".vitru-dato").fadeOut(200,function(){
+		$(".vitru-dato img").attr("src","https://embed.eltiempo.digital/infografias/2019/04/davinci/desktop/img/influencias/DaVinci_milan.png")
+	$(".vitru-dato h6").html("Milán");
+	$(".vitru-dato p").html("");
+	$(".vitru-dato").fadeIn(200);
+		
+	});
+}
+function moverCentro(){
+	$(".vitru-dato").fadeOut(200);
+	$(".vitru-dato-inicial").fadeIn(200);
+	$(".vitru-centro").animate({"top":"0%","left":"0%"},200);
+	
+}
+
+function mostrarLightbox(){
+	var urlImg=$(this).attr("data-num");
+	$(".light-obra-cont").fadeIn(300);
+	$(".light-img-cont").css("background","url(https://embed.eltiempo.digital/infografias/2019/04/davinci/desktop/img/fotos-timeline/fondo"+urlImg+".jpg) no-repeat right");
+    $(".light-obra-texto h6").html(obrasTimeline[urlImg]["nombre"]);
+    $(".light-obra-texto p").html(obrasTimeline[urlImg]["descripcion"]);
+	$(".light-img-cont").css("background-size","contain");
+}
+function ocultarLightbox(){
+	$(".light-obra-cont").fadeOut(300);
+}
+
+function moverScrollInfluenciasIzq(){
+    console.log("moverIzq");
+    console.log(scrollNum);
+    if(scrollNum<=1){
+       scrollNum=1;
+       }
+    else if(scrollNum>1){
+        scrollNum--;
+            scrollInfluencias(scrollNum);
+            }
+}
+
+function moverScrollInfluenciasDer(){
+    console.log("moverDer");
+    console.log(scrollNum);
+    if(scrollNum>=3){
+       scrollNum=3;
+       }
+    else if(scrollNum<3){
+        scrollNum++;
+            scrollInfluencias(scrollNum);
+        
+            }
+}
+
+
+function scrollInfluencias(btn){
+	var scrollCirc = $(".scroll-btn[data-num='"+btn+"']");
+	scrollCirc.parent().children(".scroll-btn").removeClass("scroll-btn-activo");
+	if(btn==1){
+		scrollCirc.parent().parent().children(".influencia-img").children("img").animate({"marginLeft":"50px"},500);
+	}
+	else if(btn==2){
+		scrollCirc.parent().parent().children(".influencia-img").children("img").animate({"marginLeft":"-420px"},500);
+	}
+	else if(btn==3){
+		scrollCirc.parent().parent().children(".influencia-img").children("img").animate({"marginLeft":"-992px"},500);
+	}
+	scrollCirc.addClass("scroll-btn-activo");
+}
+
+function influenciaSelect(){
+    scrollInfluencias(1);
+    scrollNum=1;
+	$(this).parent().children(".btn-influencia").removeClass("btn-influencia-activo");
+	var area = $(this).attr("data-area");
+	var num = $(this).attr("data-num");
+	$(this).parent().children(".influencia-img").children("img").attr("src","https://embed.eltiempo.digital/infografias/2019/04/davinci/desktop/img/influencias/"+area+"-"+num+".png");
+	$(this).addClass("btn-influencia-activo");
+}
+function mostrarVitru(){
+    $(".pop-vitru").fadeIn(200);
+}
+function ocultarVitru(){
+    $(".pop-vitru").fadeOut(200);
+}
+
+function mostrarInedit(){
+    $(".pop-inedit").fadeIn(200);
+}
+function ocultarInedit(){
+    $(".pop-inedit").fadeOut(200);
+}
